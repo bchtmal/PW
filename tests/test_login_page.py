07cb1beae3dca33b0994a8b994dd78d2674384f5
HOME-PC\alex <bchtmal@gmail.com>
2023-07-10 09:33:05 +0300

    login page tests add

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,5 +1,6 @@
 import pytest
 import pages
+import time
 
 
 class TestFooter:
@@ -29,3 +30,29 @@ class TestFooter:
         pages.index_page.open_index_page(page)
         link_locator = page.locator('span.link')
         assert link_locator.is_visible(), "Button 'Войти' not found or not visible."
+
+
+    #Проверка на наличие кнопки "Восстановить" и текст кнопки
+    def test_open_restore_password_page(self,page):
+        pages.index_page.open_index_page(page)
+        page.get_by_text("Забыли пароль?").click()
+        button_locator = page.locator('span.btn', has_text ="Восстановить" )
+        assert button_locator.is_visible(), 'Expected button to be visible, but it is not'
+
+    # Проверка на наличие ссылки "Войти" и текст ссылки
+    def test_restore_page_login_link(self, page):
+        pages.index_page.open_index_page(page)
+        page.get_by_text("Забыли пароль?").click()
+        button_locator = page.locator('span.btn.empty', has_text="Войти")
+        assert button_locator.is_visible(), 'Expected button to be visible, but it is not'
+
+    #Проверка на возврат на страницу логина при нажатии "войти" из восстановления пароля
+    def test_return_on_login_page(self, page):
+        pages.index_page.open_index_page(page)
+        page.get_by_text("Забыли пароль?").click()
+        button=page.locator('span.btn.empty', has_text="Войти")
+        button.click()
+        link_locator = page.locator('span.link')
+        link_text = link_locator.inner_text()
+        assert link_text == "Забыли пароль?", f'Expected link text to be "Забыли пароль?", but got "{link_text}"'
+
